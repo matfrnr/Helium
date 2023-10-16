09e9268e50cdab30a513f04af1bc4c0383148975
Mathéo <97743381+matfrnr@users.noreply.github.com>
2023-10-16 20:53:10 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+
+window.addEventListener("mousemove", (e) => {
+  cursor.style.top = e.y + "px";
+  cursor.style.left = e.x + "px";
+
+  mouse1.style.top = e.y + "px";
+  mouse1.style.left = e.x + "px";
+
+  mouse2.style.top = e.y + "px";
+  mouse2.style.left = e.x + "px";
+});
